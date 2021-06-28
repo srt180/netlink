420c0160c68b823ce4821d5d8f7333ae67ca0a6b
shao <shaoruntao@netbank.cn>
2021-06-28 13:39:39 +0800

    feat: add more xfrm monitor support

--- xfrm_monitor_linux.go
+++ xfrm_monitor_linux.go
@@ -1,6 +1,8 @@
 package netlink
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 
 	"github.com/vishvananda/netlink/nl"
@@ -29,6 +31,76 @@ func parseXfrmMsgExpire(b []byte) *XfrmMsgExpire {
 	e.Hard = msg.Hard == 1
 
 	return &e
+}
+
+//
+type XfrmMsgSa struct {
+	nl.XfrmUsersaInfo
+	XfrmState *XfrmState
+	saType    nl.XfrmMsgType
+}
+
+func (sa *XfrmMsgSa) Type() nl.XfrmMsgType {
+	return sa.saType
+}
+
+func parseXfrmMsgSa(b []byte, t nl.XfrmMsgType) *XfrmMsgSa {
+	var msg = nl.XfrmUsersaInfo{}
+	binary.Read(bytes.NewReader(b[0:nl.SizeofXfrmUsersaInfo]), nl.NativeEndian(), &msg)
+
+	var stat *XfrmState
+	stat, _ = parseXfrmState(b, FAMILY_ALL)
+
+	return &XfrmMsgSa{
+		XfrmUsersaInfo: msg,
+		XfrmState:      stat,
+		saType:         t,
+	}
+}
+
+//
+type XfrmUserSaId struct {
+	SaId   *nl.XfrmUsersaId
+	saType nl.XfrmMsgType
+}
+
+func (saId *XfrmUserSaId) Type() nl.XfrmMsgType {
+	return saId.saType
+}
+
+func parseXfrmMsgSaId(b []byte, t nl.XfrmMsgType) *XfrmUserSaId {
+	saId := nl.DeserializeXfrmUsersaId(b)
+
+	return &XfrmUserSaId{
+		SaId:   saId,
+		saType: t,
+	}
+}
+
+//
+type XfrmPolicy__ struct {
+	Policy *XfrmPolicy
+	t      nl.XfrmMsgType
+}
+
+func (p *XfrmPolicy__) Type() nl.XfrmMsgType {
+	return p.t
+}
+
+//
+type XfrmUserpolicyId__ struct {
+	Id *nl.XfrmUserpolicyId
+	t  nl.XfrmMsgType
+}
+
+func (id *XfrmUserpolicyId__) Type() nl.XfrmMsgType {
+	return id.t
+}
+func parseXfrmUserpolicyId(b []byte, t nl.XfrmMsgType) *XfrmUserpolicyId__ {
+	return &XfrmUserpolicyId__{
+		Id: nl.DeserializeXfrmUserpolicyId(b),
+		t:  t,
+	}
 }
 
 func XfrmMonitor(ch chan<- XfrmMsg, done <-chan struct{}, errorChan chan<- error,
@@ -64,9 +136,27 @@ func XfrmMonitor(ch chan<- XfrmMsg, done <-chan struct{}, errorChan chan<- error
 				return
 			}
 			for _, m := range msgs {
+				//fmt.Printf("msg type: %x\n", m.Header.Type)
+
 				switch m.Header.Type {
 				case nl.XFRM_MSG_EXPIRE:
 					ch <- parseXfrmMsgExpire(m.Data)
+				case nl.XFRM_MSG_NEWSA, nl.XFRM_MSG_UPDSA:
+					ch <- parseXfrmMsgSa(m.Data, nl.XfrmMsgType(m.Header.Type))
+				case nl.XFRM_MSG_DELSA:
+					ch <- parseXfrmMsgSaId(m.Data, nl.XfrmMsgType(m.Header.Type))
+				case nl.XFRM_MSG_NEWPOLICY, nl.XFRM_MSG_UPDPOLICY:
+					policy, err := parseXfrmPolicy(m.Data, FAMILY_ALL)
+					if err != nil {
+						errorChan <- err
+					} else {
+						ch <- &XfrmPolicy__{
+							Policy: policy,
+							t:      nl.XfrmMsgType(m.Header.Type),
+						}
+					}
+				case nl.XFRM_MSG_DELPOLICY:
+					ch <- parseXfrmUserpolicyId(m.Data, nl.XfrmMsgType(m.Header.Type))
 				default:
 					errorChan <- fmt.Errorf("unsupported msg type: %x", m.Header.Type)
 				}
@@ -90,6 +180,10 @@ func xfrmMcastGroups(types []nl.XfrmMsgType) ([]uint, error) {
 		switch t {
 		case nl.XFRM_MSG_EXPIRE:
 			group = nl.XFRMNLGRP_EXPIRE
+		case nl.XFRM_MSG_NEWSA, nl.XFRM_MSG_DELSA, nl.XFRM_MSG_UPDSA:
+			group = nl.XFRMNLGRP_SA
+		case nl.XFRM_MSG_NEWPOLICY, nl.XFRM_MSG_DELPOLICY, nl.XFRM_MSG_UPDPOLICY:
+			group = nl.XFRMNLGRP_POLICY
 		default:
 			return nil, fmt.Errorf("unsupported group: %x", t)
 		}
